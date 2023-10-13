341db6fe8380de9e31283a5e685b26c40a99fe5a
Hatem902 <hatelm902@gmail.com>
2023-10-13 16:17:55 +0100

    shore: removed '@ts-expect-error' above <SideBar />

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -7,7 +7,6 @@ import SideBar from "@/components/side-bar";
 export default async function BestInRole() {
   return (
     <main className="-my-2 ml-4   flex w-full max-w-fit space-x-1.5 xxl:ml-0 desktop:my-4">
-      {/* @ts-expect-error Server Component */}
       <SideBar />
       {roles.map((role) => (
         <RoleColumn role={role} key={role}></RoleColumn>
